ccb278018a9f3f88a3de838664c0b2f5eb59a273
Shu Zou <u6768067@anu.edu.au>
2020-12-11 17:21:07 +0800

    temple

--- src/xyz/chengzi/aeroplanechess/controller/GameController.java
+++ src/xyz/chengzi/aeroplanechess/controller/GameController.java
@@ -11,6 +11,7 @@ import xyz.chengzi.aeroplanechess.view.ChessBoardComponent;
 import xyz.chengzi.aeroplanechess.view.ChessComponent;
 import xyz.chengzi.aeroplanechess.view.SquareComponent;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,11 +21,11 @@ public class GameController implements InputListener, Listenable<GameStateListen
     private final ChessBoard model;
 
 
-
     private Integer rolledNumber0; // Record the last rolling outcome
     private Integer rolledNumber1;
     private Integer rolledNumber;
     private int currentPlayer;
+    private int numberRound = 1;
 
     public GameController(ChessBoardComponent chessBoardComponent, ChessBoard chessBoard) {
         this.view = chessBoardComponent;
@@ -79,32 +80,46 @@ public class GameController implements InputListener, Listenable<GameStateListen
 
     @Override
     public void onPlayerClickChessPiece(ChessBoardLocation location, ChessComponent component) {
-        if(this.model.landed_Planes[currentPlayer] == 4){
+        if (this.model.landed_Planes[currentPlayer] == 4) {
             nextPlayer();
         }
-        if (rolledNumber != null) {
+        if (rolledNumber != null && rolledNumber0 != null && rolledNumber1 != null) {
             ChessPiece piece = model.getChessPieceAt(location);
             if (piece.getPlayer() == currentPlayer) {
-                if (location.getIndex() >18 && location.getColor() == currentPlayer && !(rolledNumber0 == 6 || rolledNumber1 == 6)) {
+                if (location.getIndex() > 18 && location.getColor() == currentPlayer && !(rolledNumber0 == 6 || rolledNumber1 == 6)) {
                     System.out.println("Need a 6 to land");
-                }else
-                if(location.getIndex() >18 && location.getColor() == currentPlayer && (rolledNumber0 == 6 || rolledNumber1 == 6)){
-                    if(this.model.landed_Planes[currentPlayer] + this.model.onTheBoardPlanes[currentPlayer]==4){
+                } else if (location.getIndex() > 18 && location.getColor() == currentPlayer && (rolledNumber0 == 6 || rolledNumber1 == 6)) {
+                    if (this.model.landed_Planes[currentPlayer] + this.model.onTheBoardPlanes[currentPlayer] == 4) {
                         System.out.println("Sorry, you can only have 4 planes on the board and handars");
-                    }else{
-                        model.moveChessPiece(location,6);
+                    } else {
+                        model.moveChessPiece(location, 6);
+                        rolledNumber = null;
                     }
-                }else {
+                } else {
                     model.moveChessPiece(location, rolledNumber);
+                    rolledNumber = null;
                 }
+//                if(rolledNumber0 + rolledNumber1 >=10){
+//                    listenerList.forEach(listener -> listener.onPlayerEndRound(currentPlayer));
+//                    for (int i = 0; i <4 ; i++) {
+//                        nextPlayer();
+//                    }
+//                    listenerList.forEach(listener -> listener.onPlayerStartRound(currentPlayer));
+//                    numberRound ++;
+//                }else{
+//                    numberRound = 1;
+//                }
+//
                 listenerList.forEach(listener -> listener.onPlayerEndRound(currentPlayer));
-//                FIXME: here we need to have 3 chances in dice
                 nextPlayer();
                 listenerList.forEach(listener -> listener.onPlayerStartRound(currentPlayer));
-            }else{
+            } else {
                 System.out.println("It is not your turn !");
-                System.out.println("There is "+this.model.getGridAt(location).number_Of_Planes+" Planes");
+                System.out.println("There is " + this.model.getGridAt(location).number_Of_Planes + " Planes");
             }
+        } else {
+            JOptionPane.showMessageDialog(null, "Please Roll the Dice",
+                    "Notification", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
